865dee039662705e266928603c266478bc4e0452
anson <anson.rosenthal@gmail.com>
2017-12-03 14:58:12 +0000

    prettify

--- src/jb/core.clj
+++ src/jb/core.clj
@@ -4,7 +4,7 @@
 
 (defn get-type
   "I: any
-   O: representation of the Type of v"
+  O: representation of the Type of v"
   [v]
   (when (some? v)
     (-> v type .getName)))
@@ -15,49 +15,54 @@
 (defn combine-node
   "Merges two schemas for the same property"
   [{t-in :type r-in :required} {t-new :type r-new :required}]
-  (cond 
-    (and (map? t-in) (map? t-new)) {:type (combine t-in t-new)
-                                    :required (and r-in r-new)}
-    (and (vector? t-in) (vector? t-new)) {:type (combine (first t-in) (first t-new))
-                                          :required (and r-in r-new)}
-    (or (set? t-in) (set? t-new)) {:type (clojure.set/union (if (set? t-in) t-in #{t-in})
-                                                            (if (set? t-new) t-new #{t-new}))
-                                   :required (and r-in r-new)}
-    (= t-in t-new) {:type t-in
-                    :required (and r-in r-new)}
-    :else {:type #{t-in t-new}
-           :required (and r-in r-new)}))
+  (let [r (and r-in r-new)
+        t (cond 
+            (and (map? t-in) 
+                 (map? t-new))    (combine t-in t-new)
+            (and (vector? t-in) 
+                 (vector? t-new)) (combine (first t-in) (first t-new))
+            (or (set? t-in) 
+                (set? t-new))     (clojure.set/union (if (set? t-in) t-in #{t-in})
+                                                     (if (set? t-new) t-new #{t-new}))
+            (= t-in t-new)        t-in
+            :else                 #{t-in t-new})]
+    {:type t :required r}))
 
 (defn combine
   "Merges two Schemas for the same object"
   [m1 m2]
   (reduce (fn [m k]
             (let [node-new (get m2 k)
-                  node-in (get m k)]
-              (cond 
-                (and (some? node-in) (some? node-new)) (assoc m k (combine-node node-in node-new))
-                ;; Only not required if m1 is nontrivial
-                (some? node-new) (assoc m k (assoc node-new :required (empty? m1)))
+                  node-in (get m k)
+                  combined (cond 
 
-                ;; Existing key isn't in new node, so not required
-                (some? node-in)  (assoc m k (assoc node-in :required false)))))
+                             ;; Combine if both nontrivial 
+                             (and (some? node-in) (some? node-new)) (combine-node node-in node-new)
+
+                             ;; Only not required if m1 doesn't have key *and* is nontrivial
+                             (some? node-new)  (assoc node-new :required (empty? m1))
+
+                             ;; Existing key isn't in new node, so not required
+                             (some? node-in)  (assoc node-in :required false)
+
+                             ;; this can't happen!!
+                             :else (throw (Exception. "Unreachable code has been reached!")))]
+              (assoc m k combined)))
           m1
           (set (concat (keys m1) (keys m2)))))
 
 (defn infer
   "Runs schema-inference on the parsed JSON object
-   I: map
-   O: map, in a recursive Schema description type"
+  I: map
+  O: a Schema descriptor map"
   [m]
   (reduce (fn [acc k]
-            (let [v (acc k)]
-              (cond
-                (map? v)  (assoc acc k {:type (infer v)
-                                        :required true})
-                (coll? v) (assoc acc k {:type [(reduce combine {} (map infer v))]
-                                        :required true})
-                :else (assoc acc k {:type (-> (acc k) get-type)
-                                    :required true}))))
+            (let [v (acc k)
+                  t (cond
+                      (map? v)  (infer v)
+                      (coll? v) [(reduce combine {} (map infer v))]
+                      :else     (->> acc k get-type))]
+              (assoc acc k {:type t :required true})))
           m
           (keys m)))
 
--- test/jb/core_test.clj
+++ test/jb/core_test.clj
@@ -4,38 +4,38 @@
 
 (deftest tdd
   (testing "empty"
-    (is (= nil (browse nil))))
+    (is (= nil (infer nil))))
 
   (testing "single"
     (is (= {:foo {:type "java.lang.Long" :required true}}
-           (browse {:foo 1}))))
+           (infer {:foo 1}))))
 
   (testing "two keys, different types"
     (is (= {:foo {:type "java.lang.Long" :required true}
             :bar {:type "java.lang.String" :required true}}
-           (browse {:foo 1 :bar "baz"}))))
+           (infer {:foo 1 :bar "baz"}))))
 
   (testing "empty array"
     (is (= {:qux {:type [{}] :required true}}
-           (browse {:qux []}))))
+           (infer {:qux []}))))
 
   (testing "array with object"
     (is (= {:qux {:type [{:foo {:type "java.lang.Long" :required true}}]
                   :required true}}
-           (browse {:qux [{:foo 1}]}))))
+           (infer {:qux [{:foo 1}]}))))
 
   (testing "array with multiple objects, field sparsity"
     (is (= {:qux {:type [{:foo {:type "java.lang.Long" :required false}
                           :bar {:type "java.lang.String" :required false}}]
                   :required true}}
-           (browse {:qux [{:foo 1} {:bar "bar"}]}))))
+           (infer {:qux [{:foo 1} {:bar "bar"}]}))))
 
   (testing "nested object"
     (is (= {:qux {:type {:foo {:type "java.lang.Long" :required true}} :required true}}
-           (browse {:qux {:foo 1}}))))
+           (infer {:qux {:foo 1}}))))
 
   (testing "array with nested object, field sparsity"
     (is (= {:qux {:type [{:foo {:type {:bar {:type "java.lang.String" :required false}}
                                 :required true}}]
                   :required true}}
-           (browse {:qux [{:foo {:bar "1"}} {:foo {}}]})))))
+           (infer {:qux [{:foo {:bar "1"}} {:foo {}}]})))))
